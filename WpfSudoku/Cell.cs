a7f6a466022ce9a23b744ab5070c053dfe595f3f
scherzer <scherzer@hs-weingarten.de>
2020-08-29 18:38:44 +0200

    Add project files.

---
+++ WpfSudoku/Cell.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfSudoku
+{
+	public class Cell : INotifyPropertyChanged
+	{
+
+		bool _readOnly = false;
+		public bool ReadOnly
+		{
+			get => _readOnly;
+			set => Set(ref _readOnly, value);
+		}
+
+		int _value = 0;
+		public int Value
+		{
+			get => _value;
+			set => Set(ref _value, value);
+		}
+
+		bool _isValid = true;
+		public bool IsValid
+		{
+			get => _isValid;
+			set => Set(ref _isValid, value);
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void Set<Value>(ref Value backendStore, Value value, [CallerMemberName] string propertyName = "") where Value : IEquatable<Value>
+		{
+			if (!backendStore.Equals(value))
+			{
+				backendStore = value;
+				InvokePropertyChanged(propertyName);
+			}
+		}
+
+		private void InvokePropertyChanged([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+}
