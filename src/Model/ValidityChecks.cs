bca5bca31f7a8a92366b20e7927882b0681b3ac5
scherzer <scherzer@hs-weingarten.de>
2020-09-03 10:48:15 +0200

    quick commit

--- src/Model/ValidityChecks.cs
+++ src/Model/ValidityChecks.cs
@@ -18,18 +18,32 @@ namespace WpfSudoku.Model
 			{
 				for (int b1 = 0; b1 < 3; ++b1)
 				{
-					var used = new bool[9];
+					var used = new int[9];
+					var firstUse = new (int, int)[9];
 					for (int x = b0 * 3; x < 3 + b0 * 3; ++x)
 					{
 						for (int y = b1 * 3; y < 3 + b1 * 3; ++y)
 						{
 							var value = board[x, y];
 							if (0 == value) continue;
-							if (used[value - 1])
+							if (0 == used[value - 1])
 							{
+								firstUse[value - 1] = (x, y);
+								++used[value - 1];
+							}
+							else
+							{
+								++used[value - 1];
 								yield return (x, y);
 							}
-							used[value - 1] = true;
+						}
+					}
+					// also yield return first cell of multiple occurrence
+					for (int i = 0; i < 9; ++i)
+					{
+						if (1 < used[i])
+						{
+							yield return firstUse[i];
 						}
 					}
 				}
@@ -40,16 +54,30 @@ namespace WpfSudoku.Model
 		{
 			for (int x = 0; x < 9; ++x)
 			{
-				var used = new bool[9];
+				var used = new int[9];
+				var firstUse = new int[9];
 				for (int y = 0; y < 9; ++y)
 				{
 					var value = board[x, y];
 					if (0 == value) continue;
-					if (used[value - 1])
+					if (0 == used[value - 1])
 					{
+						firstUse[value - 1] = y;
+						++used[value - 1];
+					}
+					else
+					{
+						++used[value - 1];
 						yield return (x, y);
 					}
-					used[value - 1] = true;
+				}
+				// also yield return first cell of multiple occurrence
+				for (int i = 0; i < 9; ++i)
+				{
+					if (1 < used[i])
+					{
+						yield return (x, firstUse[i]);
+					}
 				}
 			}
 		}
@@ -58,16 +86,30 @@ namespace WpfSudoku.Model
 		{
 			for (int y = 0; y < 9; ++y)
 			{
-				var used = new bool[9];
+				var used = new int[9];
+				var firstUse = new int[9];
 				for (int x = 0; x < 9; ++x)
 				{
 					var value = board[x, y];
 					if (0 == value) continue;
-					if (used[value - 1])
+					if (0 == used[value - 1])
 					{
+						firstUse[value - 1] = x;
+						++used[value - 1];
+					}
+					else
+					{
+						++used[value - 1];
 						yield return (x, y);
 					}
-					used[value - 1] = true;
+				}
+				// also yield return first cell of multiple occurrence
+				for (int i = 0; i < 9; ++i)
+				{
+					if (1 < used[i])
+					{
+						yield return (firstUse[i], y);
+					}
 				}
 			}
 		}
--- src/View/MainWindow.xaml.cs
+++ src/View/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
-using System.Windows.Media;
 using WpfSudoku.ViewModel;
 
 namespace WpfSudoku.View
